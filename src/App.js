a4f9cf359292fa1b7c0eaced0dc1adccb49df428
Raúl Caballero (Ru\o) <racaballeroa@hotmail.com>
2018-12-07 17:00:23 -0600

    Se trata de representar el ejercicio en clase

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          // en el ejempolo que pusiste en clase fue de la siguiente manera:
+          // <p>{ 'La multiplicación de:'} ${ this.props.primero } {'y'}  ${ this.props.segundo } {'es:'}</p>
+          // pusiste un $ en cada props y eso lo pinto y en tu ejemplo no, eso por qué es?
+          <p>{ 'La multiplicación de:'} { this.props.primero } {'y'}  { this.props.segundo } {'es:'}</p>
+          <p>{ this.props.primero * this.props.segundo }</p>
+          <p>{ this.props.mensaje }</p>
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App
+  primero = {2}
+  segundo = {2}
+  mensaje = 'Me esta costando un huevo y el otro hacer código'
+  />
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

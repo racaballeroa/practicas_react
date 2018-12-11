16ac290698c71869cdf837653850e0cacb8ec8ad
Raúl Caballero (Ru\o) <racaballeroa@hotmail.com>
2018-12-11 12:47:42 -0600

    Se creo un archivo nuevo para colocar el componente navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import './example.js';
 
 // la clase App se extiende a Componente y componente es:
 class App extends Component {
@@ -21,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <Example/>
           <img src={logo} className="App-logo" alt="logo" />
           {/*en el ejempolo que pusiste en clase fue de la siguiente manera:
           <p>{ 'La multiplicación de:'} ${ this.props.primero } {'y'}
@@ -39,60 +28,6 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
-    );
-  }
-}
-class Example extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-    };
-  }
-  
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
-  
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Example from './example.js';
 
 ReactDOM.render(<App
   primero = {2}
